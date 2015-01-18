3b53c84fad6d055019909c1aac22bde9f8b96aa2
Nicole <nasdelrosario@gmail.com>
2015-01-18 11:34:11 +0100

    Now saves and retrieves database info

--- app/routes.php
+++ app/routes.php
@@ -14,17 +14,45 @@ use Carbon\Carbon;
 |
 */
 
+
 Route::get('/', function()
 {
-	$start_date = Carbon::create(2014, 10, 25);
+	return View::make('index', [
+		]);
+});
+
+
+Route::post('submit_accept', function()
+{
+
+    $start_date = Input::get('start_date');
+    $end_date = Input::get('end_date');
+    $created_by = 'nicole';
+    $created_on = date("Y-m-d");
+
+    $instace = DB::insert("INSERT INTO appointments (start_date, end_date, created_by, created_on) VALUES (?, ?, ?, ?)", [$start_date, $end_date, $created_by, $created_on]);
+    $instance = DB::select("SELECT * FROM appointments WHERE start_date = ?", [$start_date]);
+    $instance = $instance[0];
+
+
+	return View::make('submit_accept', [
+		'start_date' =>$start_date,
+		]);
+
+});
+
+
+Route::get('result', function()
+{
+	$start_date = Carbon::create(2015, 1, 05);
 	$today_date = Carbon::now();
-	$end_date = Carbon::create(2015, 7, 14);
+	$end_date = Carbon::create(2015, 2, 12);
 	$totes_wait = $start_date->diffInDays($end_date);
 	$since_then = $start_date->diffInDays($today_date);
 	$until_then = $end_date->diffInDays($today_date);
 	$percentage_done = ($since_then/$totes_wait)*100;
 
-	return View::make('index', [
+	return View::make('result', [
 		'start_date' =>$start_date,
 		'today_date' =>$today_date,
 		'end_date' =>$end_date,
@@ -33,8 +61,6 @@ Route::get('/', function()
 		'until_then' =>$until_then,
 		'percentage_done' =>$percentage_done,
 		]);
-
 });
-
 
 
---
+++ app/views/submit_accept.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>It's the final countdown!</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+
+<body>
+
+<h1>Hey there.</h1>
+
+<h1>We gots it. :)</h1>
+
+
+
+
+
+</body>
+</html>
