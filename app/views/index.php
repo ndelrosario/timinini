51726e8e6ef41d9c62c5e88f640ca3e9086639ab
Nicole <nasdelrosario@gmail.com>
2015-01-18 11:35:59 +0100

    New index page

---
+++ app/views/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>It's the final countdown!</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <form method="POST" action="<?php echo url('submit_accept') ?>" id="search_form">
+      Start Date: <input id="start_date" type="date" name="start_date" class="form-control"><br>
+      End Date:  <input id="end_date" type="date" name="end_date" class="form-control">
+      <input type="submit" value="Submit" class="btn btn-default"><br><br>
+  </form>
+
+
+
+
+</body>
+</html>
