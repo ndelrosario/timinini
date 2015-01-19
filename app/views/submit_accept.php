d4cff075efe9cf59d726426559db4e75dd76151f
Nicole <nasdelrosario@gmail.com>
2015-01-19 12:56:51 +0100

    Fixed copy

--- app/views/submit_accept.php
+++ app/views/submit_accept.php
@@ -13,8 +13,8 @@
 <h1>Hey there. We gots it. :)</h1>
 
 <h1>You want to begin counting down from <?php echo $start_date ?> and you want to count down to <?php echo $end_date ?>.</h1>
-<h1>That's <?php echo $until_then ?> more days to go.</h1>
-
+<h1>That's <?php echo $until_then ?> more days from today.</h1>
+<br><br>
 <div class="progress">
   <div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $percentage_done ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $percentage_done ?>%;">
   <?php echo number_format((float)$percentage_done, 2, '.', ''); ?>% there!
